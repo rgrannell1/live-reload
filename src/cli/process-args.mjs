4b28d48cf886044b6e6737e6b865bb502eed9593
rgrannell1 <r.grannell2@gmail.com>
2019-12-25 13:16:35 +0000

    removed unneeded config

--- src/cli/process-args.mjs
+++ src/cli/process-args.mjs
@@ -1,6 +1,4 @@
 
-import * as fs from 'fs'
-import * as path from 'path'
 import errors from '@rgrannell/errors'
 import constants from '../shared/constants'
 const { codes } = constants
@@ -8,82 +6,11 @@ import jsonSchema from 'jsonschema'
 
 const processArgs = {}
 
-/**
- * Process and validate port arguments supplied.
- *
- * @param {string} port0Arg the first port argument
- * @param {string} port1Arg the second port argument
- *
- * @returns {number} the first port argument, processed
- */
-processArgs.port = (port0Arg, port1Arg) => {
-  const port0 = parseInt(port0Arg, 10)
-  const port1 = parseInt(port1Arg, 10)
-
-  for (const port of [port0, port1]) {
-    if (isNaN(port)) {
-      throw errors.invalidPort(`${port} port was invalid`, constants.codes.LR_007)
-    }
-
-    if (port <= 0 || port > 65535) {
-      throw errors.invalidPort(`port ${port} out of allowed range`, constants.codes.LR_007)
-    }
-  }
-
-  if (port0 === port1) {
-    throw errors.identicalPorts('http and wss ports cannot be the same', constants.codes.LR_007)
-  }
-
-  return port0
-}
-
-/**
- * process the build argument supplied to live-reload
- *
- * @param {string} buildArg the shell-script to run for a build.
- *
- * @returns {string} the build argument
- */
-processArgs.build = async buildArg => {
-  const hasBuildArg = buildArg && buildArg.length > 0
-
-  if (hasBuildArg) {
-    return buildArg
-  }
-
-  const packagePath = path.join(process.cwd(), 'package.json')
-
-  try {
-    await new Promise((resolve, reject) => {
-      fs.exists(packagePath, resolve)
-    })
-  } catch (err) {
-    throw errors.buildMissing('no build command provided, and no "package.json" file found in current directory', codes.LR_001)
-  }
-
-  const pkg = require(packagePath)
-
-  if (!pkg.hasOwnProperty('scripts')) {
-    throw errors.buildMissing('no build command provided, and "package.json" in current directory has no scripts section', codes.LR_002)
-  }
-
-  if (!pkg.scripts.hasOwnProperty('build')) {
-    throw errors.buildMissing('no build command provided, and "package.json" in current directory has no build script', codes.LR_003)
-  }
-
-  return pkg.scripts.build
-}
-
-processArgs.site = siteArg => {
-  return siteArg
-}
-
-processArgs.publicDir = publicDirArg => {
-  return path.resolve(publicDirArg)
-}
-
 const schemas = {}
 
+/*
+ * the configuration for a build item
+ */
 schemas.buildItem = {
   anyOf: [
     {
@@ -107,6 +34,9 @@ schemas.buildItem = {
   ]
 }
 
+/*
+ * The expected api schema
+ */
 schemas.api = {
   type: 'object',
   required: ['path', 'watch'],
@@ -131,6 +61,9 @@ schemas.api = {
   }
 }
 
+/*
+ * The expected site schema
+ */
 schemas.site = {
   type: 'object',
   required: ['path', 'publicDir', 'watch'],
@@ -165,6 +98,9 @@ schemas.site = {
   }
 }
 
+/*
+ * The expected package json schema
+ */
 const packageSchema = {
   id: '/package',
   type: 'object',
@@ -186,6 +122,13 @@ const packageSchema = {
   }
 }
 
+/**
+ * Ensure the package.json contains the expected content
+ *
+ * @param {Object} packageJson the package.json configuration
+ *
+ * @returns {Object} configuration
+ */
 processArgs.package = packageJson => {
   if (!packageJson['live-reload']) {
     throw errors.missingConfig('no "live-reload" configuration found in package.json', codes.MISSING_CONFIG)
